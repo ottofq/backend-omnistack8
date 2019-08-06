7f61755dda47e7ecbe517b5b15a7c56c301b9dda
Otto Freitas <ottofq@gmail.com>
2019-08-06 20:29:33 -0300

    Update gitignore

--- src/server.js
+++ src/server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const cors = require("cors");
-
+const { user, pass } = require("./credentials");
 const server = express();
 
 mongoose.connect(
-  "mongodb+srv://omnistack:rkQS3ycLY6vY4BV1@cluster0-aosmr.mongodb.net/omnistack8?retryWrites=true&w=majority",
+  `mongodb+srv://${user}:${pass}@cluster0-aosmr.mongodb.net/omnistack8?retryWrites=true&w=majority`,
   { useNewUrlParser: true }
 );
 
